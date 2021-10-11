7f6476a1fa82580de56c1ea7ee58c15b3433f59f
Michael <edelsteinmichael23@gmail.com>
2021-10-11 10:28:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+// 1
+let multiply 
+const num1 = 31
+const num2 = 2
+multiply = num1 * num2
+// 2
+let random
+random = Math.round(Math.random() * 10) 
+// 3
+let mod
+const num3 = 16
+const num4 = 4
+mod = num3 / num4
+// 4
+let max
+max = Math.max(5, 10, 15, 20)
+
